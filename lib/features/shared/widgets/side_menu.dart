701c82682c92d4ab23baef0e7e4c63e716de8391
Gino Baptista <gb.g@live.com>
2024-06-06 13:24:03 -0400

    first commit

---
+++ lib/features/shared/widgets/side_menu.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:curriculum/features/shared/shared.dart';
+
+import '../../../config/router/app_router.dart';
+
+class SideMenu extends StatefulWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const SideMenu({super.key, required this.scaffoldKey});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  int navDrawerIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+    final textStyles = Theme.of(context).textTheme;
+
+    return NavigationDrawer(
+        elevation: 1,
+        selectedIndex: navDrawerIndex,
+        onDestinationSelected: (value) {
+          setState(() {
+            navDrawerIndex = value;
+          });
+
+          // final menuItem = appMenuItems[value];
+          // context.push( menuItem.link );
+          widget.scaffoldKey.currentState?.closeDrawer();
+        },
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, hasNotch ? 0 : 20, 16, 0),
+            child: Text('Saludos', style: textStyles.titleMedium),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 16, 10),
+            child: Text('Nombre User', style: textStyles.titleSmall),
+          ),
+          const NavigationDrawerDestination(
+            icon: Icon(Icons.home_outlined),
+            label: Text('Convocatorias'),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Text('Otras opciones'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: CustomFilledButton(
+                onPressed: () {
+                  appRouter.go('/login');
+                },
+                text: 'Cerrar sesión'),
+          ),
+        ]);
+  }
+}
